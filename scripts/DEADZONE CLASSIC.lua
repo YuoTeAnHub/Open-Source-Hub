07e6e34fd89e75e0206f38370a8a59cc44df344d
YuoTeAn <yuotean12@gmail.com>
2026-06-28 23:19:56 +0700

    Update DEADZONE CLASSIC.lua

--- scripts/DEADZONE CLASSIC.lua
+++ scripts/DEADZONE CLASSIC.lua
@@ -1,4 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/Bypasses/DEADZONE%20CLASSIC.lua"))()
+--// loadstring(game:HttpGet("https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/Bypasses/DEADZONE%20CLASSIC.lua"))()
+--// You can delete "--//" up and can use any walkspeed and jumppower
 
 local env = (getgenv and getgenv()) or _G
 
