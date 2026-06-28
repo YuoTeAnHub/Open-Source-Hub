ab7c99ded3064785b7b48d22168232aff389a485
YuoTeAn <yuotean12@gmail.com>
2026-06-28 15:18:56 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,7 @@
 local allowedPlaces = {
     [6788434697] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/AniPhobia.lua",
     [139432668432124] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
+    [106741147406362] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
     [3221241066] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/DEADZONE%20CLASSIC.lua",
 }
 
