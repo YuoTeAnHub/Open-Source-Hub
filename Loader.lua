1056b0e4641ab16c489f7195b001e1e561c7cdaa
YuoTeAn <yuotean12@gmail.com>
2026-06-28 15:31:42 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,23 @@
-local allowedPlaces = {
+local placeScripts = {
     [6788434697] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/AniPhobia.lua",
-    [139432668432124] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
-    [10123332358] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
     [3221241066] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/DEADZONE%20CLASSIC.lua",
+    [139432668432124] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
 }
 
-local url = allowedPlaces[game.PlaceId]
+local universeScripts = {
+    [10123332358] = "https://raw.githubusercontent.com/YuoTeAnHub/Open-Source-Hub/refs/heads/main/scripts/CLEAR.lua",
+}
+
+local url = placeScripts[game.PlaceId] or universeScripts[game.GameId]
+
 if url then
     loadstring(game:HttpGet(url))()
 else
-    warn("Open Source Hub: Game Not Supported")
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Open Source Hub",
+            Text = "Game Not Supported",
+            Duration = 5
+        })
+    end)
 end
